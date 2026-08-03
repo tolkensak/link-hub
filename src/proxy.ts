c4309c7b08832643ae64744d6b0e4f164977d5ec
Tolkyn <tolken.sak@gmail.com>
2026-08-04 03:18:11 +0500

    After middleware-to-proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,10 @@
-// src/middleware.ts
+// src/proxy.ts
 
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 import { NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
     const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
     const { pathname } = request.nextUrl;
 
