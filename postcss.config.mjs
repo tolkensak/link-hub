08a450053f416e92fb00ab6d70d17e306141e260
Tolkyn <tolken.sak@gmail.com>
2026-08-15 01:37:00 +0500

    Fix auth

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,11 @@
+// postcss.config.mjs
+
+/** @type {import('postcss-load-config').Config} */
+
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
